a1704401193a3c445aabdf4cc906e8034e60d26f
TOOMdaMoa <mailtotoom@gmail.com>
2019-09-11 21:43:29 +0200

    gitignore updated initial setup & project plan

---
+++ data_generation.py
@@ -0,0 +1,5 @@
+# code used to generate data using theoretical models.
+
+# try different Random Walk with Varying Transition Probabilities definitions
+# different lambdas, starting probability, number of steps, multiple times with same starting variables
+# save into .csv?
---
+++ model_fitting.py
@@ -0,0 +1,7 @@
+# code used to analyze generalized data
+
+# load data
+# try different possible models
+# find the best suitable model & parameter values
+# compare with reality
+
