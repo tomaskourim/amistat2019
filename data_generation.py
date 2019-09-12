5534e62984b7071d8490ca51e21e134fce6f92ff
TOOMdaMoa <mailtotoom@gmail.com>
2019-09-12 20:53:50 +0200

    universal probability computation function

--- data_generation.py
+++ data_generation.py
@@ -11,16 +11,42 @@ def ising(bernoulli: int) -> int:
     Transfers variable form Bernoulli random to Ising random; see
     https://en.wikipedia.org/wiki/Bernoulli_distribution
     https://en.wikipedia.org/wiki/Ising_model
+
+    :param bernoulli: int
+    :return:
     """
     if bernoulli == 1:
         return 1
     elif bernoulli == 0:
         return -1
     else:
-        raise Exception('Unexpected value of Bernoulli distribution')
+        raise Exception(f'Unexpected value of Bernoulli distribution: {bernoulli}')
 
 
-def generate_rw_success_punished(starting_probability: float, c_lambda: float, walk_steps: int, repetitions: int) -> \
+def next_probability(walk_type: str, c_lambdas: List[float], steps: List[int], probabilities: List[float],
+                     i: int) -> float:
+    """
+        Computes the next step transition probability according to the definition of given :param walk_type
+    :param walk_type: str
+    :param c_lambdas: List[float]
+    :param steps: List[int]
+    :param probabilities: List[float]
+    :param i: int
+    :return:
+    """
+    if walk_type == 'success_punished':
+        return c_lambdas[0] * probabilities[i - 1] + 0.5 * (1 - c_lambdas[0]) * (1 - steps[i])
+    elif walk_type == 'success_rewarded':
+        return c_lambdas[0] * probabilities[i - 1] + 0.5 * (1 - c_lambdas[0]) * (1 + steps[i])
+    elif walk_type == 'success_punished_two_lambdas':
+        return 0.5 * ((1 + steps[i]) * c_lambdas[0] * probabilities[i - 1] + (1 - steps[i]) * (
+                1 - c_lambdas[1] * (1 - probabilities[i - 1])))
+    else:
+        raise Exception(f'Unexpected walk type: {walk_type}')
+
+
+def generate_rw(walk_type: str, starting_probability: float, c_lambdas: List[float], walk_steps: int,
+                repetitions: int) -> \
         List[List[int]]:
     walks = []
     for j in range(0, repetitions):
@@ -28,43 +54,10 @@ def generate_rw_success_punished(starting_probability: float, c_lambda: float, w
         probabilities = [starting_probability]
         for i in range(1, walk_steps + 1):
             steps.append(ising(np.random.binomial(1, probabilities[i - 1], 1)[0]))  # next step using actual probability
-            probabilities.append(
-                c_lambda * probabilities[i - 1] + 0.5 * (1 - c_lambda) * (1 - steps[i]))  # as in Definition
+            probabilities.append(next_probability(walk_type, c_lambdas, steps, probabilities, i))
         walks.append(steps)
-
-    return walks
-
-
-def generate_rw_success_rewarded(starting_probability: float, c_lambda: float, walk_steps: int, repetitions: int) -> \
-        List[List[int]]:
-    walks = []
-    for j in range(0, repetitions):
-        steps = ['']
-        probabilities = [starting_probability]
-        for i in range(1, walk_steps + 1):
-            steps.append(ising(np.random.binomial(1, probabilities[i - 1], 1)[0]))  # next step using actual probability
-            probabilities.append(
-                c_lambda * probabilities[i - 1] + 0.5 * (1 - c_lambda) * (1 + steps[i]))  # as in Definition
-        walks.append(steps)
-    return walks
-
-
-def generate_rw_success_punished_two_lambdas(starting_probability: float, c_lambda0: float, c_lambda1: float,
-                                             walk_steps: int, repetitions: int) -> \
-        List[List[int]]:
-    walks = []
-    for j in range(0, repetitions):
-        steps = ['']
-        probabilities = [starting_probability]
-        for i in range(1, walk_steps + 1):
-            steps.append(ising(np.random.binomial(1, probabilities[i - 1], 1)[0]))  # next step using actual probability
-            probabilities.append(
-                0.5 * ((1 + steps[i]) * c_lambda0 * probabilities[i - 1] + (1 - steps[i]) * (
-                        1 - c_lambda1 * (1 - probabilities[i - 1]))))  # as in Definition
         print(probabilities)
         print(steps)
-        walks.append(steps)
-
     return walks
 
 
@@ -73,13 +66,17 @@ def main():
     # different lambdas, starting probability, number of steps, multiple times with same starting variables
     # save into .csv?
 
-    c_lambda0 = 0.5
-    c_lambda1 = 0.1
+    c_lambdas = [0.5]
     p0 = 0.5
     steps = 10
-    repetitions = 8
+    repetitions = 3
 
-    walks = generate_rw_success_punished_two_lambdas(p0, c_lambda0, c_lambda1, steps, repetitions)
+    walks = generate_rw('success_punished', p0, c_lambdas, steps, repetitions)
+    print(walks)
+    walks = generate_rw('success_rewarded', p0, c_lambdas, steps, repetitions)
+    print(walks)
+    c_lambdas = [0.5, 0.1]
+    walks = generate_rw('success_punished_two_lambdas', p0, c_lambdas, steps, repetitions)
     print(walks)
 
 
