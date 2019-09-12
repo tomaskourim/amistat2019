7a85d4f3eec136d14864feb01478e92283c7eb77
TOOMdaMoa <mailtotoom@gmail.com>
2019-09-12 13:59:31 +0200

    logger added

--- data_generation.py
+++ data_generation.py
@@ -3,3 +3,33 @@
 # try different Random Walk with Varying Transition Probabilities definitions
 # different lambdas, starting probability, number of steps, multiple times with same starting variables
 # save into .csv?
+import logging
+
+if __name__ == '__main__':
+    # Create a custom logger
+    logger = logging.getLogger()
+    logger.setLevel('DEBUG')
+
+    # Create handlers
+    total_handler = logging.FileHandler('logfile_total.log', mode='w')
+    info_handler = logging.FileHandler('logfile_info.log')
+    error_handler = logging.FileHandler('logfile_error.log')
+    stdout_handler = logging.StreamHandler()
+
+    total_handler.setLevel(logging.DEBUG)
+    info_handler.setLevel(logging.INFO)
+    error_handler.setLevel(logging.WARNING)
+    stdout_handler.setLevel(logging.WARNING)
+
+    # Create formatters and add it to handlers
+    logging_format = logging.Formatter('%(asctime)s - %(process)d - %(levelname)s - %(name)s - %(message)s')
+    total_handler.setFormatter(logging_format)
+    info_handler.setFormatter(logging_format)
+    error_handler.setFormatter(logging_format)
+    stdout_handler.setFormatter(logging_format)
+
+    # Add handlers to the logger
+    logger.addHandler(total_handler)
+    logger.addHandler(info_handler)
+    logger.addHandler(error_handler)
+    logger.addHandler(stdout_handler)
