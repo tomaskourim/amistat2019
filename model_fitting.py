5ec30db444e5c92dbfee407c1f85059528ecdaff
Tomáš Kouřim <kourim@outlook.com>
2019-09-15 20:48:23 +0200

    result analysis version 1

--- model_fitting.py
+++ model_fitting.py
@@ -13,7 +13,7 @@ import pandas as pd
 import scipy.optimize as opt
 
 from common import get_current_probability
-from config import DATA_DIRNAME
+from config import DATA_DIRNAME, CONFIDENCE_INTERVAL_SIZE
 
 
 # compare with reality
@@ -90,7 +90,10 @@ def get_lambda_estimate(walk_type: str, starting_probability: float, walks: List
         logging.debug("Fitted successfully.")
         return opt_result.x
     else:
-        return -10000
+        if walk_type == 'success_punished' or walk_type == 'success_rewarded':
+            return -10000
+        elif walk_type == 'success_punished_two_lambdas' or walk_type == 'success_rewarded_two_lambdas':
+            return [-10000, -10000]
 
 
 # lambda known, get p0
@@ -165,11 +168,76 @@ def get_model_estimate(walks: List[List[int]]) -> Tuple[List[float], str]:
     return result, current_model
 
 
-def analyze_result(result: pd.DataFrame):
-    print(result)
-    # CI parametru
+def analyze_result_single_lambda(result: pd.DataFrame, prediction_type: str, model_type: str):
+    lambdas = result['c_lambda'].unique()
+    p0s = result['p0'].unique()
+    step_sizes = result['step_count'].unique()
+    tries = 0
+    successes = 0
+    for p0 in p0s:
+        for step_size in step_sizes:
+            for c_lambda in lambdas:
+                result_of_test = result[
+                    result.c_lambda.isin([c_lambda]) & result.p0.isin([p0]) & result.step_count.isin([step_size])]
+                if len(result_of_test) > 1:
+                    raise Exception(f"Result is supposed to be of size 1, but is of size {len(result)}")
+                if prediction_type in ["only_lambda", "all_parameters"]:
+                    tries = tries + 1
+                    prediction = result_of_test.predicted_lambda.values[0]
+                    if c_lambda * (1 - CONFIDENCE_INTERVAL_SIZE / 2) <= prediction <= c_lambda * (
+                            1 + CONFIDENCE_INTERVAL_SIZE / 2):
+                        successes = successes + 1
+                if prediction_type in ["only_p0", "all_parameters"]:
+                    tries = tries + 1
+                    prediction = result_of_test.predicted_p0.values[0]
+                    if p0 * (1 - CONFIDENCE_INTERVAL_SIZE / 2) <= prediction <= p0 * (
+                            1 + CONFIDENCE_INTERVAL_SIZE / 2):
+                        successes = successes + 1
+                if prediction_type in ["everything"]:
+                    tries = tries + 1
+                    if result_of_test.predicted_model.values[0] == model_type:
+                        successes = successes + 1
+    print(f"Model type: {model_type}, prediction type: {prediction_type}. Success rate: {successes / tries}")
 
-    pass
+
+def analyze_result_multiple_lambda(result, prediction_type, model_type):
+    lambdas0 = result['c_lambda0'].unique()
+    lambdas1 = result['c_lambda1'].unique()
+    p0s = result['p0'].unique()
+    step_sizes = result['step_count'].unique()
+    tries = 0
+    successes = 0
+    for p0 in p0s:
+        for step_size in step_sizes:
+            for c_lambda0 in lambdas0:
+                for c_lambda1 in lambdas1:
+                    result_of_test = result[
+                        result.c_lambda0.isin([c_lambda0]) & result.c_lambda1.isin([c_lambda1]) & result.p0.isin(
+                            [p0]) & result.step_count.isin([step_size])]
+                    if len(result_of_test) > 1:
+                        raise Exception(f"Result is supposed to be of size 1, but is of size {len(result)}")
+                    if prediction_type in ["only_lambda", "all_parameters"]:
+                        tries = tries + 1
+                        prediction = result_of_test.predicted_lambda0.values[0]
+                        if c_lambda0 * (1 - CONFIDENCE_INTERVAL_SIZE / 2) <= prediction <= c_lambda0 * (
+                                1 + CONFIDENCE_INTERVAL_SIZE / 2):
+                            successes = successes + 1
+                        tries = tries + 1
+                        prediction = result_of_test.predicted_lambda1.values[0]
+                        if c_lambda1 * (1 - CONFIDENCE_INTERVAL_SIZE / 2) <= prediction <= c_lambda1 * (
+                                1 + CONFIDENCE_INTERVAL_SIZE / 2):
+                            successes = successes + 1
+                    if prediction_type in ["only_p0", "all_parameters"]:
+                        tries = tries + 1
+                        prediction = result_of_test.predicted_p0.values[0]
+                        if p0 * (1 - CONFIDENCE_INTERVAL_SIZE / 2) <= prediction <= p0 * (
+                                1 + CONFIDENCE_INTERVAL_SIZE / 2):
+                            successes = successes + 1
+                    if prediction_type in ["everything"]:
+                        tries = tries + 1
+                        if result_of_test.predicted_model.values[0] == model_type:
+                            successes = successes + 1
+    print(f"Model type: {model_type}, prediction type: {prediction_type}. Success rate: {successes / tries}")
 
 
 def analyze_results(results: pd.DataFrame):
@@ -177,64 +245,132 @@ def analyze_results(results: pd.DataFrame):
     prediction_types = results['prediction_type'].unique()
     for model_type in model_types:
         for prediction_type in prediction_types:
-            analyze_result(results[results.model_type == model_type & results.prediction_type == prediction_type])
+            if model_type == 'success_punished' or model_type == 'success_rewarded':
+                analyze_result_single_lambda(
+                    results[results.model_type.isin([model_type]) & results.prediction_type.isin([prediction_type])],
+                    prediction_type, model_type)
+            elif model_type == 'success_punished_two_lambdas' or model_type == 'success_rewarded_two_lambdas':
+                analyze_result_multiple_lambda(
+                    results[results.model_type.isin([model_type]) & results.prediction_type.isin([prediction_type])],
+                    prediction_type, model_type)
+            else:
+                raise Exception(f'Unexpected walk type: {model_type}')
 
 
 def main():
     generated_data = [f for f in listdir(DATA_DIRNAME) if isfile(join(DATA_DIRNAME, f))]
     results = pd.DataFrame(
-        columns=["model_type", "lambda", "p0", "step_count", "prediction_type", "predicted_model", "predicted_lambda",
+        columns=["model_type", "c_lambda", "c_lambda0", "c_lambda1", "p0", "step_count", "prediction_type",
+                 "predicted_model", "predicted_lambda", "predicted_lambda0", "predicted_lambda1",
                  "predicted_p0"])
     start_time_loop = datetime.now()
+    # with open("results.pkl", 'rb') as f:
+    #     results = pickle.load(f)  # load data
     for i, datafile in enumerate(generated_data):  # iterate over all generated cases
         start_time_iter = datetime.now()
         with open(join(DATA_DIRNAME, datafile), 'rb') as f:
             walks, walk_type, starting_probability, c_lambdas, step_count = pickle.load(f)  # load data
             print(walk_type, starting_probability, c_lambdas, step_count)
-            for walk in walks:
-                estimated_lambda = get_lambda_estimate(walk_type, starting_probability, [walk])
-                current_result = {"model_type": walk_type,
-                                  "lambda": c_lambdas,
-                                  "p0": starting_probability,
-                                  "step_count": step_count,
-                                  "prediction_type": "only_lambda",
-                                  "predicted_model": "",
-                                  "predicted_lambda": estimated_lambda,
-                                  "predicted_p0": ""}
-                results = results.append(current_result, ignore_index=True)
+            if walk_type == 'success_punished' or walk_type == 'success_rewarded':
+                c_lambda = c_lambdas[0]
+                c_lambda0 = ""
+                c_lambda1 = ""
+            elif walk_type == 'success_punished_two_lambdas' or walk_type == 'success_rewarded_two_lambdas':
+                c_lambda = ""
+                c_lambda0 = c_lambdas[0]
+                c_lambda1 = c_lambdas[1]
+            else:
+                raise Exception(f'Unexpected walk type: {walk_type}')
 
-                estimated_p0 = get_p0_estimate(walk_type, c_lambdas, [walk])
-                current_result = {"model_type": walk_type,
-                                  "lambda": c_lambdas,
-                                  "p0": starting_probability,
-                                  "step_count": step_count,
-                                  "prediction_type": "only_p0",
-                                  "predicted_model": "",
-                                  "predicted_lambda": "",
-                                  "predicted_p0": estimated_p0}
-                results = results.append(current_result, ignore_index=True)
+            estimated_lambdas = get_lambda_estimate(walk_type, starting_probability, walks)
+            if walk_type == 'success_punished' or walk_type == 'success_rewarded':
+                estimated_lambda = estimated_lambdas
+                estimated_lambda0 = ""
+                estimated_lambda1 = ""
+            elif walk_type == 'success_punished_two_lambdas' or walk_type == 'success_rewarded_two_lambdas':
+                estimated_lambda = ""
+                estimated_lambda0 = estimated_lambdas[0]
+                estimated_lambda1 = estimated_lambdas[1]
+            else:
+                raise Exception(f'Unexpected walk type: {walk_type}')
+            current_result = {"model_type": walk_type,
+                              "c_lambda": c_lambda,
+                              "c_lambda0": c_lambda0,
+                              "c_lambda1": c_lambda1,
+                              "p0": starting_probability,
+                              "step_count": step_count,
+                              "prediction_type": "only_lambda",
+                              "predicted_model": "",
+                              "predicted_lambda": estimated_lambda,
+                              "predicted_lambda0": estimated_lambda0,
+                              "predicted_lambda1": estimated_lambda1,
+                              "predicted_p0": ""}
+            results = results.append(current_result, ignore_index=True)
 
-                estimated_params = get_parameters_estimate(walk_type, [walk])
-                current_result = {"model_type": walk_type,
-                                  "lambda": c_lambdas,
-                                  "p0": starting_probability,
-                                  "step_count": step_count,
-                                  "prediction_type": "all_parameters",
-                                  "predicted_model": "",
-                                  "predicted_lambda": estimated_params[1:],
-                                  "predicted_p0": estimated_params[0]}
-                results = results.append(current_result, ignore_index=True)
+            estimated_p0 = get_p0_estimate(walk_type, c_lambdas, walks)
+            current_result = {"model_type": walk_type,
+                              "c_lambda": c_lambda,
+                              "c_lambda0": c_lambda0,
+                              "c_lambda1": c_lambda1,
+                              "p0": starting_probability,
+                              "step_count": step_count,
+                              "prediction_type": "only_p0",
+                              "predicted_model": "",
+                              "predicted_lambda": "",
+                              "predicted_lambda0": "",
+                              "predicted_lambda1": "",
+                              "predicted_p0": estimated_p0}
+            results = results.append(current_result, ignore_index=True)
 
-                estimated_params, estimated_model = get_model_estimate([walk])
-                current_result = {"model_type": walk_type,
-                                  "lambda": c_lambdas,
-                                  "p0": starting_probability,
-                                  "step_count": step_count,
-                                  "prediction_type": "everything",
-                                  "predicted_model": estimated_model,
-                                  "predicted_lambda": estimated_params[1:],
-                                  "predicted_p0": estimated_params[0]}
-                results = results.append(current_result, ignore_index=True)
+            estimated_params = get_parameters_estimate(walk_type, walks)
+            if walk_type == 'success_punished' or walk_type == 'success_rewarded':
+                estimated_lambda = estimated_params[1]
+                estimated_lambda0 = ""
+                estimated_lambda1 = ""
+            elif walk_type == 'success_punished_two_lambdas' or walk_type == 'success_rewarded_two_lambdas':
+                estimated_lambda = ""
+                estimated_lambda0 = estimated_params[1]
+                estimated_lambda1 = estimated_params[2]
+            else:
+                raise Exception(f'Unexpected walk type: {walk_type}')
+            current_result = {"model_type": walk_type,
+                              "c_lambda": c_lambda,
+                              "c_lambda0": c_lambda0,
+                              "c_lambda1": c_lambda1,
+                              "p0": starting_probability,
+                              "step_count": step_count,
+                              "prediction_type": "all_parameters",
+                              "predicted_model": "",
+                              "predicted_lambda": estimated_lambda,
+                              "predicted_lambda0": estimated_lambda0,
+                              "predicted_lambda1": estimated_lambda1,
+                              "predicted_p0": estimated_params[0]}
+            results = results.append(current_result, ignore_index=True)
+
+            estimated_params, estimated_model = get_model_estimate(walks)
+            if walk_type == 'success_punished' or walk_type == 'success_rewarded':
+                estimated_lambda = estimated_params[1]
+                estimated_lambda0 = ""
+                estimated_lambda1 = ""
+            elif walk_type == 'success_punished_two_lambdas' or walk_type == 'success_rewarded_two_lambdas':
+                estimated_lambda = ""
+                estimated_lambda0 = estimated_params[1]
+                estimated_lambda1 = estimated_params[2] if len(estimated_params) == 3 else -10000
+            else:
+                raise Exception(f'Unexpected walk type: {walk_type}')
+            current_result = {"model_type": walk_type,
+                              "c_lambda": c_lambda,
+                              "c_lambda0": c_lambda0,
+                              "c_lambda1": c_lambda1,
+                              "p0": starting_probability,
+                              "step_count": step_count,
+                              "prediction_type": "everything",
+                              "predicted_model": estimated_model,
+                              "predicted_lambda": estimated_lambda,
+                              "predicted_lambda0": estimated_lambda0,
+                              "predicted_lambda1": estimated_lambda1,
+                              "predicted_p0": estimated_params[0]}
+            results = results.append(current_result, ignore_index=True)
 
             end_time_iter = datetime.now()
             time_curr = end_time_iter - start_time_iter
@@ -242,6 +378,8 @@ def main():
             eta = (len(generated_data) - i - 1) * time_per_iter
             logging.info(f"Current iteration: {time_curr}. Per iter {time_per_iter}. ETA: {eta}")
 
+    with open("results.pkl", 'wb') as f:
+        pickle.dump([results], f)
     analyze_results(results)
 
 
