0e7ab788b4e23e9b07c2c63a01664c7d6e65446c
Tomáš Kouřim <kourim@outlook.com>
2020-02-13 14:03:33 +0100

    remove old functions

--- results_analysis.py
+++ results_analysis.py
@@ -12,109 +12,10 @@ from config import C_LAMBDAS, START_PROBABILITIES, STEP_COUNTS, C_LAMBDA_PAIRS, 
     PREDICTION_TYPES
 
 
-def parameter_estimate_evaluation(true_parameter_value: float, prediction: float, successes: int) -> int:
-    if true_parameter_value * (1 - CONFIDENCE_INTERVAL_SIZE / 2) <= prediction <= true_parameter_value * (
-            1 + CONFIDENCE_INTERVAL_SIZE / 2):
-        successes = successes + 1
-    return successes
-
-
-def model_estimate_evaluation(true_model_type: str, model_prediction: str, successes: int) -> int:
-    if true_model_type == model_prediction:
-        successes = successes + 1
-    return successes
-
-
 def check_prediction(prediction: float, model_type: str, prediction_type: str, true_value: float):
     if prediction >= 1 or prediction <= 0:
         logging.error(
             f"Wrong prediction {prediction}. Model {model_type}, Predi_type {prediction_type}, true val {true_value}")
-
-
-def analyze_result_single_lambda(result: pd.DataFrame, prediction_type: str, model_type: str):
-    lambdas = result['c_lambda'].unique()
-    p0s = result['p0'].unique()
-    step_counts = result['step_count'].unique()
-    tries = 0
-    successes = 0
-    for p0 in p0s:
-        if p0 < 0.5:
-            continue
-        for step_count in step_counts:
-            if step_count < 5:
-                continue
-            for c_lambda in lambdas:
-                if c_lambda < 0.5:
-                    continue
-                result_of_test = result[
-                    result.c_lambda.isin([c_lambda]) & result.p0.isin([p0]) & result.step_count.isin([step_count])]
-                if len(result_of_test) > 1:
-                    raise Exception(f"Result is supposed to be of size 1, but is of size {len(result)}")
-                if prediction_type in ["only_lambda", "all_parameters"]:
-                    tries = tries + 1
-                    prediction = result_of_test.predicted_lambda.values[0]
-                    check_prediction(prediction, model_type, prediction_type, c_lambda)
-                    successes = parameter_estimate_evaluation(c_lambda, prediction, successes)
-                if prediction_type in ["only_p0", "all_parameters"]:
-                    tries = tries + 1
-                    prediction = result_of_test.predicted_p0.values[0]
-                    check_prediction(prediction, model_type, prediction_type, p0)
-                    successes = parameter_estimate_evaluation(p0, prediction, successes)
-                if prediction_type in ["everything"]:
-                    tries = tries + 1
-                    successes = model_estimate_evaluation(model_type, result_of_test.predicted_model.values[0],
-                                                          successes)
-    logging.info(
-        f"Model type: {model_type}, prediction type: {prediction_type}. Successes: {successes}. Tries: {tries}. Success rate: {successes / tries}")
-
-
-def analyze_result_multiple_lambda(result, prediction_type, model_type):
-    lambdas0 = result['c_lambda0'].unique()
-    lambdas1 = result['c_lambda1'].unique()
-    p0s = result['p0'].unique()
-    step_counts = result['step_count'].unique()
-    tries = 0
-    successes = 0
-    for p0 in p0s:
-        if p0 < 0.5:
-            continue
-        for step_count in step_counts:
-            if step_count < 5:
-                continue
-            for c_lambda0 in lambdas0:
-                if c_lambda0 < 0.5:
-                    continue
-                for c_lambda1 in lambdas1:
-                    if c_lambda1 < 0.5:
-                        continue
-                    result_of_test = result[
-                        result.c_lambda0.isin([c_lambda0]) & result.c_lambda1.isin([c_lambda1]) & result.p0.isin(
-                            [p0]) & result.step_count.isin([step_count])]
-                    if len(result_of_test) == 0:
-                        continue
-                    if len(result_of_test) > 1:
-                        raise Exception(f"Result is supposed to be of size 1, but is of size {len(result)}")
-                    if prediction_type in ["only_lambda", "all_parameters"]:
-                        tries = tries + 1
-                        prediction = result_of_test.predicted_lambda0.values[0]
-                        check_prediction(prediction, model_type, prediction_type, c_lambda0)
-                        successes = parameter_estimate_evaluation(c_lambda0, prediction, successes)
-
-                        tries = tries + 1
-                        prediction = result_of_test.predicted_lambda1.values[0]
-                        check_prediction(prediction, model_type, prediction_type, c_lambda1)
-                        successes = parameter_estimate_evaluation(c_lambda1, prediction, successes)
-                    if prediction_type in ["only_p0", "all_parameters"]:
-                        tries = tries + 1
-                        prediction = result_of_test.predicted_p0.values[0]
-                        check_prediction(prediction, model_type, prediction_type, p0)
-                        successes = parameter_estimate_evaluation(p0, prediction, successes)
-                    if prediction_type in ["everything"]:
-                        tries = tries + 1
-                        successes = model_estimate_evaluation(model_type, result_of_test.predicted_model.values[0],
-                                                              successes)
-    logging.info(
-        f"Model type: {model_type}, prediction type: {prediction_type}. Successes: {successes}. Tries: {tries}. Success rate: {successes / tries}")
 
 
 def select_results(results: pd.DataFrame, prediction_type: str, model_type: str, c_lambdas: List[float],
@@ -256,22 +157,6 @@ def analyze_results(results: pd.DataFrame):
                                                          p0)
                         fitting_results = fitting_results.append(
                             analyze_prediction_combination(current_results, columns))
-        model_types = results['model_type'].unique()
-        prediction_types = results['prediction_type'].unique()
-        for model_type in model_types:
-            for prediction_type in prediction_types:
-                if model_type == 'success_punished' or model_type == 'success_rewarded':
-                    analyze_result_single_lambda(
-                        results[
-                            results.model_type.isin([model_type]) & results.prediction_type.isin([prediction_type])],
-                        prediction_type, model_type)
-                elif model_type == 'success_punished_two_lambdas' or model_type == 'success_rewarded_two_lambdas':
-                    analyze_result_multiple_lambda(
-                        results[
-                            results.model_type.isin([model_type]) & results.prediction_type.isin([prediction_type])],
-                        prediction_type, model_type)
-                else:
-                    raise Exception(f'Unexpected walk type: {model_type}')
 
 
 def main():
