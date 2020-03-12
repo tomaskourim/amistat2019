2d7b6d0e1393b55c8076eebeddecdf2bb93f8c7f
Tomáš Kouřim <kourim@outlook.com>
2020-03-12 21:29:45 +0100

    analyze all results together

--- results_analysis.py
+++ results_analysis.py
@@ -7,13 +7,13 @@ import numpy as np
 import pandas as pd
 import scipy.stats as st
 
-from config import CONFIDENCE_INTERVAL_SIZE, MODEL_PARAMETERS, PREDICTION_VALUES, C_LAMBDAS, START_PROBABILITIES, \
-    STEP_COUNTS, C_LAMBDA_PAIRS, MODEL_TYPES, PREDICTION_TYPES, REPETITIONS_OF_WALK, REPETITIONS_OF_WALK_SERIES, \
+from config import CONFIDENCE_INTERVAL_SIZES, MODEL_PARAMETERS, PREDICTION_VALUES, C_LAMBDAS, START_PROBABILITIES, \
+    STEP_COUNTS, C_LAMBDA_PAIRS, MODEL_TYPES, PREDICTION_TYPES, REPETITIONS_OF_WALK_S, REPETITIONS_OF_WALK_SERIES, \
     OPTIMIZATION_ALGORITHM
 
 
-def check_prediction(prediction: pd.Series, model_type: str, prediction_type: str, true_value: float) -> Tuple[
-    pd.Series, int]:
+def check_prediction(prediction: pd.Series, model_type: str, prediction_type: str, true_value: float) -> \
+        Tuple[pd.Series, int]:
     not_fitted = 0
     if (prediction == 'not_fitted').any():
         not_fitted = sum(prediction == 'not_fitted')
@@ -43,16 +43,17 @@ def in_interval(datapoint: float, interval: List[float]) -> int:
     return 1 if interval[0] <= datapoint <= interval[1] else 0
 
 
-def evaluate_point_prediction(result_row: pd.DataFrame, data: pd.Series, true_value: float, name: str) -> pd.DataFrame:
+def evaluate_point_prediction(result_row: pd.DataFrame, data: pd.Series, true_value: float, name: str,
+                              confidence_interval_size: float) -> pd.DataFrame:
     data, not_fitted = check_prediction(data, result_row["model_type"][0], result_row["prediction_type"][0], true_value)
     mean = float(np.mean(data))
     median = float(np.median(data))
     stdev = float(np.std(data))
-    conf_int = st.t.interval(1 - CONFIDENCE_INTERVAL_SIZE, len(data) - 1, loc=mean, scale=st.sem(data))
-    percentile_int = [np.percentile(data, int(CONFIDENCE_INTERVAL_SIZE / 2 * 100), interpolation='midpoint'),
-                      np.percentile(data, int((1 - CONFIDENCE_INTERVAL_SIZE / 2) * 100), interpolation='midpoint')]
-    near_int = [max(0, true_value - true_value * (CONFIDENCE_INTERVAL_SIZE / 2)),
-                min(1, true_value + true_value * (CONFIDENCE_INTERVAL_SIZE / 2))]
+    conf_int = st.t.interval(1 - confidence_interval_size, len(data) - 1, loc=mean, scale=st.sem(data))
+    percentile_int = [np.percentile(data, int(confidence_interval_size / 2 * 100), interpolation='midpoint'),
+                      np.percentile(data, int((1 - confidence_interval_size / 2) * 100), interpolation='midpoint')]
+    near_int = [max(0.0, true_value - true_value * (confidence_interval_size / 2)),
+                min(1.0, true_value + true_value * (confidence_interval_size / 2))]
     conf_int_success = in_interval(true_value, conf_int)
     percentile_success = in_interval(true_value, percentile_int)
     near_mean_success = in_interval(mean, near_int)
@@ -75,24 +76,25 @@ def evaluate_point_prediction(result_row: pd.DataFrame, data: pd.Series, true_va
 
 
 def evaluate_lambda_prediction(result_row: pd.DataFrame, current_results: pd.DataFrame, model_type: str,
-                               c_lambdas: List[float]) -> pd.DataFrame:
+                               c_lambdas: List[float], confidence_interval_size: float) -> pd.DataFrame:
     if 'two_lambdas' in model_type:
         data = current_results.predicted_lambda0
         name = "lambda0"
-        result_row = evaluate_point_prediction(result_row, data, c_lambdas[1], name)
+        result_row = evaluate_point_prediction(result_row, data, c_lambdas[1], name, confidence_interval_size)
         data = current_results.predicted_lambda1
         name = "lambda1"
-        return evaluate_point_prediction(result_row, data, c_lambdas[2], name)
+        return evaluate_point_prediction(result_row, data, c_lambdas[2], name, confidence_interval_size)
     else:
         data = current_results.predicted_lambda
         name = "lambda"
-        return evaluate_point_prediction(result_row, data, c_lambdas[0], name)
+        return evaluate_point_prediction(result_row, data, c_lambdas[0], name, confidence_interval_size)
 
 
-def evaluate_p0_prediction(result_row: pd.DataFrame, current_results: pd.DataFrame, p0: float) -> pd.DataFrame:
+def evaluate_p0_prediction(result_row: pd.DataFrame, current_results: pd.DataFrame, p0: float,
+                           confidence_interval_size: float) -> pd.DataFrame:
     data = current_results.predicted_p0
     name = "p0"
-    return evaluate_point_prediction(result_row, data, p0, name)
+    return evaluate_point_prediction(result_row, data, p0, name, confidence_interval_size)
 
 
 def evaluate_model_prediction(result_row: pd.DataFrame, current_results: pd.DataFrame, model_type: str) -> pd.DataFrame:
@@ -109,7 +111,8 @@ def evaluate_model_prediction(result_row: pd.DataFrame, current_results: pd.Data
     return result_row
 
 
-def analyze_prediction_combination(current_results: pd.DataFrame, columns: dict) -> pd.DataFrame:
+def analyze_prediction_combination(current_results: pd.DataFrame, columns: dict,
+                                   confidence_interval_size: float) -> pd.DataFrame:
     # gather input parameters
     input_parameters = current_results.head(1).reset_index(drop=True)
     input_parameters.drop(columns="repetition", inplace=True)
@@ -130,22 +133,21 @@ def analyze_prediction_combination(current_results: pd.DataFrame, columns: dict)
 
     # evaluate fitting
     if prediction_type == "only_lambda" or prediction_type == "all_parameters":
-        result_row = evaluate_lambda_prediction(result_row, current_results, model_type, c_lambdas)
+        result_row = evaluate_lambda_prediction(result_row, current_results, model_type, c_lambdas,
+                                                confidence_interval_size)
     if prediction_type == "only_p0" or prediction_type == "all_parameters":
-        result_row = evaluate_p0_prediction(result_row, current_results, p0)
+        result_row = evaluate_p0_prediction(result_row, current_results, p0, confidence_interval_size)
     if prediction_type == "everything":
         result_row = evaluate_model_prediction(result_row, current_results, model_type)
     return result_row
 
 
-def analyze_results(results: pd.DataFrame):
+def analyze_results(results: pd.DataFrame, repetitions_of_walk: int):
     columns = MODEL_PARAMETERS
     columns.extend(["prediction_type", "mean_predicted_lambda", "mean_predicted_lambda0", "mean_predicted_lambda1",
                     "mean_predicted_p0", "stdev_predicted_lambda", "stdev_predicted_lambda0", "stdev_predicted_lambda1",
                     "stdev_predicted_p0", "median_predicted_lambda", "median_predicted_lambda0",
-                    "median_predicted_lambda1", "median_predicted_p0", "not_fitted_lambda_count",
-                    "not_fitted_lambda0_count", "not_fitted_lambda1_count", "not_fitted_p0_count",
-                    "not_fitted_model_count"])
+                    "median_predicted_lambda1", "median_predicted_p0"])
     columns.extend(["predicted_lambda_conf_int_LB", "predicted_lambda_conf_int_UB", "predicted_lambda_percentile_LB",
                     "predicted_lambda_percentile_UP", "predicted_lambda_near_value_LB",
                     "predicted_lambda_near_value_UP"])
@@ -166,30 +168,37 @@ def analyze_results(results: pd.DataFrame):
     columns.extend(["predicted_p0_conf_int_success", "predicted_p0_percentile_success",
                     "predicted_p0_near_value_mean_success", "predicted_p0_near_value_median_success"])
     columns.append("predicted_model_success_rate")
+    columns.extend(["not_fitted_lambda_count", "not_fitted_lambda0_count", "not_fitted_lambda1_count",
+                    "not_fitted_p0_count", "not_fitted_model_count"])
     fitting_results = pd.DataFrame(columns=columns)
 
-    for index, c_lambda in enumerate(C_LAMBDAS):
-        for p0 in START_PROBABILITIES:
-            for step_count in STEP_COUNTS:
-                for model_type in MODEL_TYPES:
-                    if 'two_lambdas' in model_type:
-                        c_lambdas = C_LAMBDA_PAIRS[index]
-                    else:
-                        c_lambdas = [c_lambda]
-                    for prediction_type in PREDICTION_TYPES:
-                        current_results = select_results(results, prediction_type, model_type, c_lambdas, step_count,
-                                                         p0)
-                        fitting_results = fitting_results.append(
-                            analyze_prediction_combination(current_results, columns))
-    fitting_results.to_excel(
-        f"fitting_evaluation_interval_size_{CONFIDENCE_INTERVAL_SIZE}_K{REPETITIONS_OF_WALK}_N{REPETITIONS_OF_WALK_SERIES}_{OPTIMIZATION_ALGORITHM}.xlsx")
+    for confidence_interval_size in CONFIDENCE_INTERVAL_SIZES:
+        for index, c_lambda in enumerate(C_LAMBDAS):
+            for p0 in START_PROBABILITIES:
+                for step_count in STEP_COUNTS:
+                    for model_type in MODEL_TYPES:
+                        if 'two_lambdas' in model_type:
+                            c_lambdas = C_LAMBDA_PAIRS[index]
+                        else:
+                            c_lambdas = [c_lambda]
+                        for prediction_type in PREDICTION_TYPES:
+                            current_results = select_results(results, prediction_type, model_type, c_lambdas,
+                                                             step_count,
+                                                             p0)
+                            fitting_results = fitting_results.append(
+                                analyze_prediction_combination(current_results, columns, confidence_interval_size))
+        fitting_results.to_excel(
+            f"fitting_evaluation_interval_size_{confidence_interval_size}_K{repetitions_of_walk}_"
+            f"N{REPETITIONS_OF_WALK_SERIES}_{OPTIMIZATION_ALGORITHM}.xlsx")
 
 
 def main():
-    with open(f"results_{OPTIMIZATION_ALGORITHM}_K{REPETITIONS_OF_WALK}_N{REPETITIONS_OF_WALK_SERIES}.pkl", 'rb') as f:
-        results = pickle.load(f)  # load data
+    for repetitions_of_walk in REPETITIONS_OF_WALK_S:
+        with open(f"results_{OPTIMIZATION_ALGORITHM}_K{repetitions_of_walk}_N{REPETITIONS_OF_WALK_SERIES}.pkl",
+                  'rb') as f:
+            results = pickle.load(f)  # load data
 
-    analyze_results(results[0])
+        analyze_results(results[0], repetitions_of_walk)
 
 
 if __name__ == '__main__':
